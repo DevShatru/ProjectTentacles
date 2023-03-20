12f3ec081c8b0d8c143c7fe4426e594d0abf7482
Shatru-Home <shatrujit95@gmail.com>
2023-03-20 12:06:09 -0700

    Blank project base

---
+++ Source/ProjectTentacle/ProjectTentacleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectTentacleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTENTACLE_API AProjectTentacleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
