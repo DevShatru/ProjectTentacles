79728f8a53ee33eadca793aa2f60503780d59576
Shatru-Home <shatrujit95@gmail.com>
2023-03-27 14:51:28 -0700

    Custom context to generate around player

---
+++ Source/ProjectTentacle/Public/Characters/Enemies/Context/EnvQueryContext_Player.h
@@ -0,0 +1,20 @@
+// Copyright (C) The Tentacle Zone 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "EnvQueryContext_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTENTACLE_API UEnvQueryContext_Player : public UEnvQueryContext
+{
+	GENERATED_BODY()
+	
+	virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const override;
+
+	static AActor* Player;
+};
