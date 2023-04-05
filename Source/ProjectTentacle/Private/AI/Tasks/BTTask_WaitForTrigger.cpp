f89889466972b6b611a0fe77ffae4c7136956f0f
Shatru-Home <shatrujit95@gmail.com>
2023-04-05 08:54:03 -0700

    Wait for trigger also updates character rotation while waiting

--- Source/ProjectTentacle/Private/AI/Tasks/BTTask_WaitForTrigger.cpp
+++ Source/ProjectTentacle/Private/AI/Tasks/BTTask_WaitForTrigger.cpp
@@ -3,6 +3,9 @@
 
 #include "AI/Tasks/BTTask_WaitForTrigger.h"
 
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
 UBTTask_WaitForTrigger::UBTTask_WaitForTrigger()
 {
 	NodeName = "Wait for Trigger";
@@ -14,5 +17,29 @@ UBTTask_WaitForTrigger::UBTTask_WaitForTrigger()
 // In progress until aborted
 EBTNodeResult::Type UBTTask_WaitForTrigger::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	Controller = OwnerComp.GetAIOwner();
+	if(!Controller) return EBTNodeResult::Failed;
+	
+	Pawn = Controller->GetPawn();
+	if(!Pawn) return EBTNodeResult::Failed;
+	
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+
+	float FlatAngle;
+	AActor* AsActor = Cast<AActor>(Blackboard->GetValueAsObject("Target"));
+	if(!AsActor) return EBTNodeResult::Failed;
+
+	FocalActor = AsActor;
+	OwnerComp.GetAIOwner()->SetFocus(FocalActor);
+	
+	FlatAngle = CalculateFlatAngle(Pawn->GetActorForwardVector(), (FocalActor->GetActorLocation() - Pawn->GetActorLocation()).GetSafeNormal());	
+	
 	return EBTNodeResult::InProgress;
+}
+
+float UBTTask_WaitForTrigger::CalculateFlatAngle(const FVector& VectorA, const FVector& VectorB)
+{
+	return (VectorA.IsNearlyZero() || VectorB.IsNearlyZero())
+		? 0.f
+		: FMath::Abs(FMath::RadiansToDegrees(acosf(VectorA.CosineAngle2D(VectorB))));
 }
