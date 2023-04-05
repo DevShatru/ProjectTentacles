1fe09ce2df241900c7f9f0607948765ac0a6b390
Shatru-Home <shatrujit95@gmail.com>
2023-04-05 12:56:54 -0700

    Changed env variable for "Program Files" location

--- CompileHook.cmd
+++ CompileHook.cmd
@@ -3,9 +3,9 @@
 :: Project name
 set SOLUTION=ProjectTentacle
 :: Default MSBuild location
-set MSBUILD=%ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe
+set MSBUILD=%ProgramW6432%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe
 :: UE4 Version Selector, used to regenerate project files in case they are missing
-set UEVersionSelect=%ProgramFiles(x86)%\Epic Games\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe
+set UEVersionSelect=%ProgramFiles%\Epic Games\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe
 
 :: If solution already exists, skip generation
 if exist %SOLUTION%.sln goto :soutiongenerated
@@ -15,11 +15,7 @@ if exist %SOLUTION%.sln goto :soutiongenerated
 
 :: Once solution is generated, run msbuild with relevant params
 :soutiongenerated
-@echo on
-echo %MSBUILD%
-echo %SOLUTION%.sln
-"%MSBUILD%" %~dp0\%SOLUTION%.sln /p:Configuration=Development /p:Platform="Win64"
-@echo off
+"%MSBUILD%" %SOLUTION%.sln /p:Configuration=Development /p:Platform="Win64"
 :: Unset env variables and turn echo back on
 set MSBUILD=
 set SOLUTION=
