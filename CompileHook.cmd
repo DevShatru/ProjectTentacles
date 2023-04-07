fdd332acae15019d32efd33df9c0b9846311e7ed
Shatru-Home <shatrujit95@gmail.com>
2023-04-07 13:05:34 -0700

    Always regenerate .sln

--- CompileHook.cmd
+++ CompileHook.cmd
@@ -7,14 +7,10 @@ set MSBUILD=%ProgramW6432%\Microsoft Visual Studio\2022\Community\MSBuild\Curren
 :: UE4 Version Selector, used to regenerate project files in case they are missing
 set UEVersionSelect=%ProgramFiles%\Epic Games\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe
 
-:: If solution already exists, skip generation
-if exist %SOLUTION%.sln goto :soutiongenerated
-
 :: Generate solution files
-"%UEVersionSelect%" /projectfiles %~dp0\%SOLUTION%.uproject
+"%UEVersionSelect%" /projectfiles %~dp0%SOLUTION%.uproject
 
 :: Once solution is generated, run msbuild with relevant params
-:soutiongenerated
 "%MSBUILD%" %SOLUTION%.sln /p:Configuration=Development /p:Platform="Win64"
 :: Unset env variables and turn echo back on
 set MSBUILD=
