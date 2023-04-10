a53f8b24fa376ae9e91a6ec894b43912746e5394
Shatru-Home <shatrujit95@gmail.com>
2023-04-10 15:22:38 -0700

    Added a count meta type to enum for ease of iteration

--- Source/ProjectTentacle/Public/Characters/Enemies/EnemyType.h
+++ Source/ProjectTentacle/Public/Characters/Enemies/EnemyType.h
@@ -7,5 +7,7 @@ enum class EEnemyType: uint8
 {
 	Melee = 0 UMETA(DisplayName = "Melee"),
 	Ranged = 1 UMETA(DisplayName = "Ranged"),
-	Brute = 2 UMETA(DisplayName = "Brute")
+	Brute = 2 UMETA(DisplayName = "Brute"),
+	Count UMETA(Hidden)
 };
+ENUM_RANGE_BY_COUNT(EEnemyType, EEnemyType::Count);
