049fc5f95a76c1dc4b91784e084539919f637c52
Shatru-Home <shatrujit95@gmail.com>
2023-05-03 09:23:22 -0700

    New enemy type for healer

--- Source/ProjectTentacle/Public/Characters/Enemies/EnemyType.h
+++ Source/ProjectTentacle/Public/Characters/Enemies/EnemyType.h
@@ -8,6 +8,7 @@ enum class EEnemyType: uint8
 	Melee = 0 UMETA(DisplayName = "Melee"),
 	Ranged = 1 UMETA(DisplayName = "Ranged"),
 	Brute = 2 UMETA(DisplayName = "Brute"),
+	Healer = 3 UMETA(DisplayName = "Healer"),
 	Count UMETA(Hidden)
 };
 ENUM_RANGE_BY_COUNT(EEnemyType, EEnemyType::Count);
