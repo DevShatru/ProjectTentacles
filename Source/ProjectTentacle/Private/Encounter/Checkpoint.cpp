5b7e67fd77678bcd9001694b9677fcef799f9eb7
Shatru-Home <shatrujit95@gmail.com>
2023-05-05 14:25:31 -0700

    Checkpoint has a trigger that disables itself and alerts the game mode when the player overlaps it

--- Source/ProjectTentacle/Private/Encounter/Checkpoint.cpp
+++ Source/ProjectTentacle/Private/Encounter/Checkpoint.cpp
@@ -3,9 +3,40 @@
 
 #include "Encounter/Checkpoint.h"
 
+#include "Characters/Player/PlayerCharacter.h"
+#include "Components/BoxComponent.h"
+#include "ProjectTentacle/ProjectTentacleGameModeBase.h"
+
 // Sets default values
 ACheckpoint::ACheckpoint()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	CheckpointTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger"));
+	CheckpointTrigger->SetupAttachment(RootComponent);
+	CheckpointTrigger->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnTrigger);
+}
+
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+	bIsActive = true;
+}
+
+void ACheckpoint::SetGameModeRef(AProjectTentacleGameModeBase* GameMode)
+{
+	GameModeRef = GameMode;
+}
+
+void ACheckpoint::OnTrigger(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(!bIsActive) return;
+	
+	if(const APlayerCharacter* PC = Cast<APlayerCharacter>(OtherActor))
+	{
+		bIsActive = false;
+		GameModeRef->SetActiveCheckpointLocation(this);
+		if(CheckpointTrigger->OnComponentBeginOverlap.IsAlreadyBound(this, &ACheckpoint::OnTrigger)) CheckpointTrigger->OnComponentBeginOverlap.RemoveDynamic(this, &ACheckpoint::OnTrigger);
+	}
 }
--- Source/ProjectTentacle/Public/Encounter/Checkpoint.h
+++ Source/ProjectTentacle/Public/Encounter/Checkpoint.h
@@ -15,4 +15,17 @@ class PROJECTTENTACLE_API ACheckpoint : public ATargetPoint
 public:	
 	// Sets default values for this actor's properties
 	ACheckpoint();
+	virtual void BeginPlay() override;
+	void SetGameModeRef(class AProjectTentacleGameModeBase* GameMode);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UBoxComponent* CheckpointTrigger;
+
+	UFUNCTION()
+	void OnTrigger(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	
+private:
+	AProjectTentacleGameModeBase* GameModeRef;
+	unsigned int bIsActive;
 };
